cdf168f1cdcbe5f7e1f0be112f3fdc7f3f6eb080
doritoes <39832079+doritoes@users.noreply.github.com>
2024-07-03 15:16:03 -0400

    Update create-database.sql

--- XCP-ng/create-database.sql
+++ XCP-ng/create-database.sql
@@ -2,4 +2,3 @@ CREATE DATABASE guac_db;
 CREATE USER 'guac_user'@'localhost' IDENTIFIED BY 'password';
 GRANT SELECT,INSERT,UPDATE,DELETE ON guac_db.* TO 'guac_user'@'localhost';
 FLUSH PRIVILEGES;
-EXIT;
