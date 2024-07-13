d0900e860e34983ea66724266caec7bf5153832c
doritoes <39832079+doritoes@users.noreply.github.com>
2024-07-12 22:45:12 -0400

    Update shared_drives.ps1

--- XCP-ng/shared_drives.ps1
+++ XCP-ng/shared_drives.ps1
@@ -1,20 +1,6 @@
 # Install-WindowsFeature -Name RSAT-AD-Powershell
 Import-Module -Name ActiveDirectory
 
-######
-##    ##
-##   ##     ##   ##    ##### #     ###
-######      #   ##    #   ###    ##   ##
-##  ###      #####    ######      ###
-##   ###                  #          ##
-######                 ###        ###
-
-# Developer has read access to all write to none
-# Public should be writable/full control by all domain users
-# Developer needs to write on own folder
-# try replacing ACL with ACL on developer, finanace, marketing
-######
-
 # Create Folders on E:\
 $folderNames = @("Marketing", "Finance", "Development", "IT", "Public")
 $rootPath = "E:\"
@@ -25,6 +11,7 @@ foreach ($folderName in $folderNames) {
 }
 
 #### Public Folder - G Drive
+# Set folder permissions
 $acl = Get-Acl E:\Public
 $identity = (Get-ADGroup -Identity "Domain Users").Sid
 $ace = New-Object System.Security.AccessControl.FileSystemAccessRule ($identity, "FullControl", "ContainerInherit,ObjectInherit", "None","Allow")
@@ -34,11 +21,13 @@ if ($acl) {
   $acl = New-Object System.Security.AccessControl.FileSystemAccessRule ($identity, "FullControl", "ContainerInherit,ObjectInherit", "None","Allow")
 }
 Set-Acl E:\Public -AclObject $acl
+# share the folder and fix permissions
 New-SmbShare -Name "Public" -Path E:\Public
 Grant-SmbShareAccess -Name "Public" -AccountName "Domain Users" -AccessRight Full -Force
 Revoke-SMBShareAccess -Name "Public" -AccountName "Everyone" -Force
 
 #### Marketing Folder - H Drive
+# create group and add users
 $groupName = "MarketingGroup"
 $groupOU = "OU=Marketing,OU=Corp,DC=xcpng,DC=lab"
 $groupPath = "$groupName,$groupOU"
@@ -53,7 +42,7 @@ $ouPath = "OU=Marketing,OU=Corp,DC=xcpng,DC=lab"
 $groupPath = "MarketingGroup,OU=Marketing,OU=Corp,DC=xcpng,DC=lab"
 $usersToAdd = Get-ADUser -Filter * -SearchBase $ouPath | Select-Object -ExpandProperty SamAccountName
 Add-ADGroupMember -Identity $groupName -Members $usersToAdd
-
+# Set folder permissions
 $acl = Get-Acl E:\Marketing
 $identity = (Get-ADGroup -Identity $groupName).Sid
 $ace = New-Object System.Security.AccessControl.FileSystemAccessRule ($identity, "FullControl", "ContainerInherit,ObjectInherit", "None","Allow")
@@ -63,11 +52,13 @@ if ($acl) {
   $acl = New-Object System.Security.AccessControl.FileSystemAccessRule ($identity, "FullControl", "ContainerInherit,ObjectInherit", "None","Allow")
 }
 Set-Acl E:\Marketing -AclObject $acl
+# share the folder and fix permissions
 New-SmbShare -Name "Marketing" -Path E:\Marketing
 Grant-SmbShareAccess -Name "Marketing" -AccountName $groupName -AccessRight Full -Force
-Revoke-SMBShareAccess -Name "Public" -AccountName "Everyone" -Force
+Revoke-SMBShareAccess -Name "Marketing" -AccountName "Everyone" -Force
 
 #### Finance Folder - I Drive
+# create group and add users
 $groupName = "FinanceGroup"
 $groupOU = "OU=Finance,OU=Corp,DC=xcpng,DC=lab"
 $groupPath = "$groupName,$groupOU"
@@ -82,7 +73,7 @@ $ouPath = "OU=Marketing,OU=Corp,DC=xcpng,DC=lab"
 $groupPath = "FinanceGroup,OU=Finance,OU=Corp,DC=xcpng,DC=lab"
 $usersToAdd = Get-ADUser -Filter * -SearchBase $ouPath | Select-Object -ExpandProperty SamAccountName
 Add-ADGroupMember -Identity $groupName -Members $usersToAdd
-
+# Set folder permissions
 $acl = Get-Acl E:\Finance
 $identity = (Get-ADGroup -Identity "FinanceGroup").Sid
 $ace = New-Object System.Security.AccessControl.FileSystemAccessRule ($identity, "FullControl", "ContainerInherit,ObjectInherit", "None","Allow")
@@ -92,11 +83,13 @@ if ($acl) {
   $acl = New-Object System.Security.AccessControl.FileSystemAccessRule ($identity, "FullControl", "ContainerInherit,ObjectInherit", "None","Allow")
 }
 Set-Acl E:\Finance -AclObject $acl
+# share the folder and fix permissions
 New-SmbShare -Name "Finance" -Path E:\Finance
 Grant-SmbShareAccess -Name "Finance" -AccountName $groupName -AccessRight Full -Force
-Revoke-SMBShareAccess -Name "Public" -AccountName "Everyone" -Force
+Revoke-SMBShareAccess -Name "Finance" -AccountName "Everyone" -Force
 
 #### Development Folder - J Drive
+# create group and add users
 $groupName = "DevelopmentGroup"
 $groupOU = "OU=Development,OU=Corp,DC=xcpng,DC=lab"
 $groupPath = "$groupName,$groupOU"
@@ -111,7 +104,7 @@ $ouPath = "OU=Development,OU=Corp,DC=xcpng,DC=lab"
 $groupPath = "DevelopmentGroup,OU=Development,OU=Corp,DC=xcpng,DC=lab"
 $usersToAdd = Get-ADUser -Filter * -SearchBase $ouPath | Select-Object -ExpandProperty SamAccountName
 Add-ADGroupMember -Identity $groupName -Members $usersToAdd
-
+# Set folder permissions
 $acl = Get-Acl E:\Development
 $identity = (Get-ADGroup -Identity "DevelopmentGroup").Sid
 $ace = New-Object System.Security.AccessControl.FileSystemAccessRule ($identity, "FullControl", "ContainerInherit,ObjectInherit", "None","Allow")
@@ -121,13 +114,13 @@ if ($acl) {
   $acl = New-Object System.Security.AccessControl.FileSystemAccessRule ($identity, "FullControl", "ContainerInherit,ObjectInherit", "None","Allow")
 }
 Set-Acl E:\Development -AclObject $acl
+# share the folder and fix permissions
 New-SmbShare -Name "Development" -Path E:\Development
 Grant-SmbShareAccess -Name "Development" -AccountName "DevelopmentGroup" -AccessRight Full -Force
 Revoke-SMBShareAccess -Name "Development" -AccountName "Everyone" -Force
 
 #### IT Folder - K Drive
-$acl = Get-Acl E:\IT
-
+# create group and add users
 $groupName = "ITGroup"
 $groupOU = "OU=Support,OU=Corp,DC=xcpng,DC=lab"
 $groupPath = "$groupName,$groupOU"
@@ -142,7 +135,8 @@ $ouPath = "OU=Support,OU=Corp,DC=xcpng,DC=lab"
 $groupPath = "ITGroup,OU=Development,OU=Corp,DC=xcpng,DC=lab"
 $usersToAdd = Get-ADUser -Filter * -SearchBase $ouPath | Select-Object -ExpandProperty SamAccountName
 Add-ADGroupMember -Identity $groupName -Members $usersToAdd
-
+# Set folder permissions
+$acl = Get-Acl E:\IT
 $ace = New-Object System.Security.AccessControl.FileSystemAccessRule ($identity, "FullControl", "ContainerInherit,ObjectInherit", "None","Allow")
 if ($acl) {
   $acl.AddAccessRule($ace)
@@ -150,7 +144,8 @@ if ($acl) {
   $acl = New-Object System.Security.AccessControl.FileSystemAccessRule ($identity, "FullControl", "ContainerInherit,ObjectInherit", "None","Allow")
 }
 Set-Acl E:\Public -AclObject $acl
+# share the folder and fix permissions
 New-SmbShare -Name "IT" -Path E:\IT
 Grant-SmbShareAccess -Name "IT" -AccountName "ITGroup" -AccessRight Full -Force
-Grant-SmbShareAccess -Name "IT" -AccountName "Domain Users" -AccessRight Full -Force
+Grant-SmbShareAccess -Name "IT" -AccountName "Domain Users" -AccessRight Read -Force
 Revoke-SMBShareAccess -Name "IT" -AccountName "Everyone" -Force
