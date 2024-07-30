2663ae2916717528d51fdbb29397241b6b9d85b8
doritoes <39832079+doritoes@users.noreply.github.com>
2024-07-29 23:03:05 -0400

    Create server-clone.tf

---
+++ proxmox/server-clone.tf
@@ -0,0 +1,28 @@
+resource "proxmox_vm_qemu" "server-clone" {
+  # VM general settings
+  target_node = "proxmox-lab"
+  name = "server-clone"
+  desc = "ubuntu server"
+
+  # onboot = true
+
+  # VM OS Settings
+  clone = "template_ubuntu_server"
+
+  # VM System Settings
+  agent = 1
+
+  # VM CPU Settings
+  cores = 2
+  sockets = 1
+  cpu = "host"
+
+  # VM Memory Settings
+  memory = 2048
+
+  # VM Network Settings
+  network {
+    bridge = "vmbr1"
+    model = "virtio"
+  }
+}
