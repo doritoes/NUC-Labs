ec7ddf564efd29510395a8788e8dcc463658b218
doritoes <39832079+doritoes@users.noreply.github.com>
2024-08-01 16:45:49 -0400

    Create provider.tf

---
+++ XCP-ng/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 0.13.0"
+  required_providers {
+    xenorchestra = {
+      source = "terra-farm/xenorchestra"
+    }
+  }
+}
+variable "xo_host" {
+  type = string
+}
+variable "xo_username" {
+  type = string
+}
+variable "xo_password" {
+  type = string
+  sensitive = true
+}
+provider "xenorchestra" {
+  url      = "ws://{{ xo_host }}"
+  username = "{{ xo_username }}"
+  password = "{{ xo_password }}"
+  insecure = true
+}
