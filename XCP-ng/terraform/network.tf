17228ecbf045368239b208aff0c6379f259aee58
doritoes <39832079+doritoes@users.noreply.github.com>
2024-08-03 15:46:24 -0400

    Update network.tf

--- XCP-ng/terraform/network.tf
+++ XCP-ng/terraform/network.tf
@@ -10,53 +10,53 @@ resource "xenorchestra_network" "vlan_network_100" {
   source_pif_device = "eth0"
   vlan = 100
 }
-# VLAN 110 ISP1 network
-resource "xenorchestra_network" "vlan_network_110" {
+# VLAN 101 ISP1 network
+resource "xenorchestra_network" "vlan_network_101" {
   name_label = "isp1"
   name_description = "isp1 emulated network"
   pool_id = data.xenorchestra_host.host1.pool_id
   source_pif_device = "eth0"
-  vlan = 110
+  vlan = 101
 }
-# VLAN 120 ISP2 network
-resource "xenorchestra_network" "vlan_network_120" {
+# VLAN 102 ISP2 network
+resource "xenorchestra_network" "vlan_network_102" {
   name_label = "isp2"
   name_description = "isp2 emulated network"
   pool_id = data.xenorchestra_host.host1.pool_id
   source_pif_device = "eth0"
-  vlan = 120
+  vlan = 102
 }
-# VLAN 130 ISP3 network
-resource "xenorchestra_network" "vlan_network_130" {
+# VLAN 103 ISP3 network
+resource "xenorchestra_network" "vlan_network_103" {
   name_label = "isp3"
   name_description = "isp3 emulated network"
   pool_id = data.xenorchestra_host.host1.pool_id
   source_pif_device = "eth0"
-  vlan = 130
+  vlan = 103
 }
-# VLAN 210 Branch1 network
-resource "xenorchestra_network" "vlan_network_210" {
+# VLAN 201 Branch1 network
+resource "xenorchestra_network" "vlan_network_201" {
   name_label = "branch1"
   name_description = "Branch 1 LAN"
   pool_id = data.xenorchestra_host.host1.pool_id
   source_pif_device = "eth0"
-  vlan = 210
+  vlan = 201
 }
-# VLAN 220 Branch2 network
-resource "xenorchestra_network" "vlan_network_220" {
+# VLAN 202 Branch2 network
+resource "xenorchestra_network" "vlan_network_202" {
   name_label = "branch2"
   name_description = "Branch 2 LAN"
   pool_id = data.xenorchestra_host.host1.pool_id
   source_pif_device = "eth0"
-  vlan = 220
+  vlan = 202
 }
-# VLAN 230 Branch3 network
-resource "xenorchestra_network" "vlan_network_230" {
+# VLAN 203 Branch3 network
+resource "xenorchestra_network" "vlan_network_203" {
   name_label = "branch3"
   name_description = "Branch 3 LAN"
   pool_id = data.xenorchestra_host.host1.pool_id
   source_pif_device = "eth0"
-  vlan = 230
+  vlan = 203
 }
 # VLAN 301 Branch1 DMZ
 resource "xenorchestra_network" "vlan_network_301" {
