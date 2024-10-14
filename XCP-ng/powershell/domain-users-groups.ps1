197e736b6e902edd6ec44590818e591abf66e18e
doritoes <39832079+doritoes@users.noreply.github.com>
2024-10-14 19:00:48 -0400

    Update domain-users-groups.ps1

--- XCP-ng/powershell/domain-users-groups.ps1
+++ XCP-ng/powershell/domain-users-groups.ps1
@@ -5,9 +5,9 @@ New-ADOrganizationalUnit -Name "Development" -Path "OU=Corp,DC=xcpng,DC=lab"
 New-ADOrganizationalUnit -Name "Sales" -Path "OU=Corp,DC=xcpng,DC=lab"
 New-ADOrganizationalUnit -Name "Marketing" -Path "OU=Corp,DC=xcpng,DC=lab"
 New-ADOrganizationalUnit -Name "Finance" -Path "OU=Corp,DC=xcpng,DC=lab"
-# Import Users from C:\domain_users.csv
+# Import Users from C:\domain-users.csv
 # CSV file must be in UTF-8 encoding
-$import_users = Import-Csv -Path "C:\domain_users.csv"
+$import_users = Import-Csv -Path "C:\domain-users.csv"
 # Create AD users with properties from CSV
 $import_users | ForEach-Object {
   # Create secure password
