4f70f5bb4afbc792d7e5efb53e12aabcf3a09bfe
doritoes <39832079+doritoes@users.noreply.github.com>
2024-10-14 18:08:50 -0400

    Update branch1-dhcp.ps1

--- XCP-ng/powershell/branch1-dhcp.ps1
+++ XCP-ng/powershell/branch1-dhcp.ps1
@@ -1,3 +1,4 @@
 Install-WindowsFeature -Name DHCP -IncludeManagementTools
+Add-DhcpServerInDC -DnsName dc-1.xcpng.lab -IPAddress 10.0.1.10
 Add-DhcpServerv4Scope -Name "branch1" -StartRange 10.0.1.20 -EndRange 10.0.1.250 -SubnetMask 255.255.255.0 -State Active
 Set-DhcpServerv4OptionValue -DnsDomain xcpng.lab -DnsServer 10.0.1.10 -Router 10.0.1.1
