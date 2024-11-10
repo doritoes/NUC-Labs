2ede5d146c9c2ac515ab5d736d1c1ded80a79b8e
doritoes <39832079+doritoes@users.noreply.github.com>
2024-11-09 22:52:04 -0500

    Update branch2-dhcp.ps1

--- XCP-ng/powershell/branch2-dhcp.ps1
+++ XCP-ng/powershell/branch2-dhcp.ps1
@@ -1,2 +1,3 @@
 Add-DhcpServerv4Scope -Name "branch2" -StartRange 10.0.2.20 -EndRange 10.0.2.250 -SubnetMask 255.255.255.0 -State Active
 Set-DhcpServerv4OptionValue -DnsDomain xcpng.lab -DnsServer 10.0.1.10 -Router 10.0.2.1
+Set-DhcpServerv4OptionValue -ScopeId (Get-DhcpServerv4Scope -ScopeID 10.0.2.0).ScopeId -DnsDomain xcpng.lab -DnsServer 10.0.1.10 -Router 10.0.2.1
