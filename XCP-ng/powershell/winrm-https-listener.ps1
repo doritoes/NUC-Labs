046f6bf613ec6d3b6ef239065016aafff05482d3
doritoes <39832079+doritoes@users.noreply.github.com>
2024-11-24 22:28:37 -0500

    Create winrm-https-listener.ps1

---
+++ XCP-ng/powershell/winrm-https-listener.ps1
@@ -0,0 +1,35 @@
+# Create self signed certificate
+$certParams = @{
+    CertStoreLocation = 'Cert:\LocalMachine\My'
+    DnsName           = $env:COMPUTERNAME
+    NotAfter          = (Get-Date).AddYears(1)
+    Provider          = 'Microsoft Software Key Storage Provider'
+    Subject           = "CN=$env:COMPUTERNAME"
+}
+$cert = New-SelfSignedCertificate @certParams
+
+# Create HTTPS listener
+$httpsParams = @{
+    ResourceURI = 'winrm/config/listener'
+    SelectorSet = @{
+        Transport = "HTTPS"
+        Address   = "*"
+    }
+    ValueSet = @{
+        CertificateThumbprint = $cert.Thumbprint
+        Enabled               = $true
+    }
+}
+New-WSManInstance @httpsParams
+
+# Opens port 5986 for all profiles
+$firewallParams = @{
+    Action      = 'Allow'
+    Description = 'Inbound rule for Windows Remote Management via WS-Management. [TCP 5986]'
+    Direction   = 'Inbound'
+    DisplayName = 'Windows Remote Management (HTTPS-In)'
+    LocalPort   = 5986
+    Profile     = 'Any'
+    Protocol    = 'TCP'
+}
+New-NetFirewallRule @firewallParams
