9c8384853a368050ead157479424692069e2fd67
doritoes <39832079+doritoes@users.noreply.github.com>
2024-12-05 21:46:43 -0500

    Update scrape-manufacturers.py

--- nautobot/scrape-manufacturers.py
+++ nautobot/scrape-manufacturers.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python3
-# if pyyaml is not installed by default: pip install pyyaml
-import fnmatch
-import json
-import yaml
+"""
+git clone https://github.com/nautobot/devicetype-library
+python3 scrape-manufacturers.py
+inputs libary of nautobot device-types
+creates output file manufacturs.csv
+"""
 import os
+import fnmatch
+import yaml
 
 file_matches = []
 manufacturers = []
-outfilename = 'manufacturers.csv'
+OUTFILENAME = 'manufacturers.csv'
 
-# file the device-type yaml files
+# find the device-type yaml files
 for root, dirnames, filenames in os.walk('devicetype-library/device-types'):
     for filename in fnmatch.filter(filenames, '*.yaml'):
         file_matches.append(os.path.join(root, filename))
-# scrape all the manufacturers
-for device_type_file in file_matches: 
-    with open(device_type_file, 'r') as f:
+# scrape all the manufacturers from the files
+for device_type_file in file_matches:
+    with open(device_type_file, 'r', encoding="utf-8") as f:
         data = yaml.load(f, Loader=yaml.SafeLoader)
         if data['manufacturer'] not in manufacturers:
             manufacturers.append(data['manufacturer'])
             print(f"Added {data['manufacturer']}")
 # dump csv of the sorted list of manufacturers
-with open(outfilename, 'w') as outfile:
+with open(OUTFILENAME, 'w', encoding="utf-8") as outfile:
     outfile.write("name\n")
     outfile.write("\n".join(str(manufacturer) for manufacturer in sorted(manufacturers)))
-print(f"Created file {outfilename} with {len(manufacturers)} manufacturers")
+print(f"Created file {OUTFILENAME} with {len(manufacturers)} manufacturers")
