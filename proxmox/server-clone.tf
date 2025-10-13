7fc7f28c4e020cca4f63c852bc7faf5aad160ecf
doritoes <39832079+doritoes@users.noreply.github.com>
2025-10-13 12:02:08 -0400

    Update server-clone.tf

--- proxmox/server-clone.tf
+++ proxmox/server-clone.tf
@@ -1,28 +1,27 @@
 resource "proxmox_vm_qemu" "server-clone" {
   # VM general settings
+  count = 2 # number of VMs to create
   target_node = "proxmox-lab"
-  vmid = "409"
-  name = "server-clone"
-  desc = "ubuntu server"
-
+  name = "server-clone-${count.index + 1}"
+  description = "ubuntu server"
   # onboot = true
 
-  # VM OS Settings
+  # Clone VM operation
   clone = "ubuntu-server-lan"
-
-  # VM System Settings
-  agent = 1
-
-  # VM CPU Settings
-  cores = 2
-  sockets = 1
-  cpu = "host"
-
-  # VM Memory Settings
+  # note that cores, sockets, and memory settings not copied form source VM template
+  cpu {
+    cores = 1
+    sockets = 1
+    type = "host"
+  }
   memory = 2048
 
-  # VM Network Settings
+  # Activate QEMU agent
+  agent = 1
+
+  # Network settings
   network {
+    id = 0
     bridge = "vmbr1"
     model = "virtio"
   }
