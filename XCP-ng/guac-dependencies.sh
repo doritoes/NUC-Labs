5851e71272c2b501e16e16b0136bdd29f85dfa51
doritoes <39832079+doritoes@users.noreply.github.com>
2025-10-23 14:36:48 -0400

    Delete XCP-ng/guac-dependencies.sh

--- XCP-ng/guac-dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-sudo apt update && sudo apt upgrade -y
-sudo apt install build-essential libcairo2-dev libjpeg-turbo8-dev \
-    libpng-dev libtool-bin uuid-dev libossp-uuid-dev libvncserver-dev \
-    freerdp2-dev libssh2-1-dev libtelnet-dev libwebsockets-dev \
-    libpulse-dev libvorbis-dev libwebp-dev libssl-dev libpulse-dev\
-    libpango1.0-dev libswscale-dev libavcodec-dev libavutil-dev \
-    libavformat-dev
