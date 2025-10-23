bd670649ba650409c394f560c04295aad0e47486
doritoes <39832079+doritoes@users.noreply.github.com>
2025-10-23 14:02:07 -0400

    Update guac-dependencies.sh

--- XCP-ng/guac-dependencies.sh
+++ XCP-ng/guac-dependencies.sh
@@ -1,19 +1,8 @@
 #!/usr/bin/env bash
 sudo apt update && sudo apt upgrade -y
 sudo apt install build-essential libcairo2-dev libjpeg-turbo8-dev \
-    libpng-dev libtool-bin libossp-uuid-dev libvncserver-dev \
+    libpng-dev libtool-bin uuid-dev libossp-uuid-dev libvncserver-dev \
     freerdp2-dev libssh2-1-dev libtelnet-dev libwebsockets-dev \
-    libpulse-dev libvorbis-dev libwebp-dev libssl-dev \
+    libpulse-dev libvorbis-dev libwebp-dev libssl-dev libpulse-dev\
     libpango1.0-dev libswscale-dev libavcodec-dev libavutil-dev \
     libavformat-dev
-    
-sudo apt install -y build-essential libcairo2-dev libjpeg-dev \
-                    libjpeg-turbo8-dev libpng-dev libtool-bin \
-                    libossp-uuid-dev libvncserver-dev \
-                    libfreerdp-dev freerdp2-dev freerdp-x11 \
-                    libssh2-1-dev libtelnet-dev libwebsockets-dev
-                    libpulse-dev libvorbis-dev libwebp-dev libssl-dev \
-                    libpango1.0-dev libswscale-dev libavcodec-dev \
-                    libavutil-dev libavformat-dev tomcat9 \
-                    tomcat9-admin tomcat9-common tomcat9-user \
-                    mariadb-server
