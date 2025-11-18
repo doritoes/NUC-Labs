f4f3f7b4c2b6316ed8ce33c74a4d193066c84312
doritoes <39832079+doritoes@users.noreply.github.com>
2025-11-18 13:15:42 -0500

    Update auth.sh

--- XCP-ng/auth.sh
+++ XCP-ng/auth.sh
@@ -2,7 +2,7 @@
 # entering your key and running this script sets up the ansible automation to work
 # example
 # scp auth.sh ansible@192.168.41.20:~/
-# ssh ansbile@192.168.41.10 "bash auth.sh"
+# ssh ansbile@192.168.41.20 "bash auth.sh"
 mkdir ~/.ssh
 chmod u=rwx,g=,o= ~/.ssh
 touch ~/.ssh/authorized_keys
